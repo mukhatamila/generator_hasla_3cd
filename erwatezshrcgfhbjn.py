d08cbc94e5eaf8542175cd5b18273cc15691c316
senfu_pych <senfurion2k@gmail.com>
2025-04-01 10:24:01 +0200

    added ex

--- erwatezshrcgfhbjn.py
+++ erwatezshrcgfhbjn.py
@@ -1,11 +1,18 @@
 import random
+def main() -> str:
+    '''
+    :param all: combines all characters
+    :param password: generates random password
+    :return: returns random password
+    '''
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "0123456789"
+    symbols = "[]{}()*;/,._-'"
 
-lower = "abcdefghijklmnopqrstuvwxyz"
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numbers = "0123456789"
-symbols = "[]{}()*;/,._-'"
-
-all = lower + upper + numbers + symbols
-length = 16
-password = "".join(random.sample(all, length))
-print(password)
+    all = lower + upper + numbers + symbols
+    length = 16
+    password = "".join(random.sample(all, length))
+    return password
+if __name__ == "__main__":
+    print(main())
