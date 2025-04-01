cfa1276092fe90d382414c9119ba8c3901fa3d07
DmytroH-AR <dmytro.hontar@uczen.akademiarzemiosla.opole.pl>
2025-04-01 10:22:17 +0200

    Update gen_haslo.py

--- gen_haslo.py
+++ gen_haslo.py
@@ -6,5 +6,6 @@ numbers = "0123456789"
 symbols = "[]{}()*;/,._-'"
 all = lower + upper + numbers + symbols
 length = 16
-password = "".join(random.sample(all,length))
+password = "".join(random.sample(all,length)) 
+# sample picks out a random symbol length amount of times from the provided string
 print(password)
